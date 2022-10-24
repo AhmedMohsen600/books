4dc852171f18568187620beaa7fa0387eb183312
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2022-10-24 19:14:44 +0200

    implement remove functionality

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const group = document.querySelector('ul');
 const addBtn = document.querySelector('.push');
-const removeBtn = document.querySelector('.remove');
 const authorInput = document.querySelector('.author');
 const titleInput = document.querySelector('.title');
 const submit = document.querySelector('form');
@@ -23,19 +22,24 @@ const booksData = [
   },
 ];
 
-let getData = JSON.parse(localStorage.getItem('books'));
-
 let content = '';
+
+const generate = (dt) => {
+  return `<li>
+    <h2>${dt.title} by ${dt.author}</h2>
+    <button class='remove'>${dt.id}</button>
+  </li>`;
+};
 
 const displayData = (data) => {
   data.forEach((book) => {
-    content += `<li>
-      <h2>${book.title} by ${book.author}</h2>
-      <button class="remove">Remove</button>
-    </li>`;
+    content += generate(book);
     group.innerHTML = content;
   });
+  content = '';
 };
+
+let getData = JSON.parse(localStorage.getItem('books'));
 
 window.addEventListener(
   'load',
@@ -45,20 +49,37 @@ window.addEventListener(
       getData = JSON.parse(localStorage.getItem('books'));
       displayData(getData);
     } else displayData(getData);
+    const removeBtn = document.querySelectorAll('.remove');
+    Array.from(removeBtn).forEach((bt) => {
+      bt.addEventListener('click', () => {
+        console.log('adsad');
+        console.log(bt);
+        removeBook();
+      });
+    });
   },
   false
 );
+
+const removeBook = () => {
+  const toto = getData.filter((element, index) => {
+    return element.id !== index;
+  });
+
+  localStorage.setItem('books', JSON.stringify(toto));
+  displayData(getData);
+};
 
 const addBook = () => {
   getData.push({
     title: titleInput.value,
     author: authorInput.value,
+    id: getData.length ? getData[getData.length - 1].id + 1 : 1,
   });
+
   localStorage.setItem('books', JSON.stringify(getData));
   displayData(getData);
 };
-
-const removeBook = (id) => {};
 
 submit.addEventListener('submit', (e) => {
   e.preventDefault();
